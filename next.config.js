ed79063d33f9407cc2a2f8df98ced5c290830152
Himanshu <himanshu3889@gmail.com>
2022-08-13 13:36:44 +0530

    refactor : next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // typescript: { ignoreBuildErrors: true },
+  typescript: { ignoreBuildErrors: true },
 
   reactStrictMode: true,
   images: {
